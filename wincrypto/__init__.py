fa71c0edd42728b16f5bee11be02f3eef79d47c8
crappycrypto <crappycrypto@xs4all.nl>
2014-08-08 21:28:38 +0200

    Import/export of RSA keys

--- wincrypto/__init__.py
+++ wincrypto/__init__.py
@@ -1,2 +1,82 @@
-def test():
-    return 'test successful'
+from collections import namedtuple
+import struct
+
+from Crypto.PublicKey import RSA
+from Crypto.Util.number import long_to_bytes, bytes_to_long, inverse
+
+
+PUBLICKEYSTRUC = namedtuple('PUBLICKEYSTRUC', 'bType bVersion aiKeyAlg')  # reserved is skipped when unpacking
+PUBLICKEYSTRUC_s = struct.Struct('<bb2xI')
+
+PRIVATEKEYBLOB = namedtuple('PRIVATEKEYBLOB', 'modulus prime1 prime2 exponent1 exponent2 coefficient privateExponent')
+
+RSAPUBKEY = namedtuple('RSAPUBKEY', 'magic bitlen pubexp')
+RSAPUBKEY_s = struct.Struct('<4sII')
+
+RSAPUBKEY_MAGIC = 'RSA1'
+PRIVATEKEYBLOB_MAGIC = 'RSA2'
+
+# bType
+bType_PUBLICKEYBLOB = 6
+bType_PRIVATEKEYBLOB = 7
+
+# CALG
+CALG_RSA_KEYX = 0xa400
+
+
+def rsa_to_publickeystruc(rsa_key):
+    n = rsa_key.key.n
+    e = rsa_key.key.e
+    n_bytes = long_to_bytes(n)[::-1]
+    result = PUBLICKEYSTRUC_s.pack(bType_PUBLICKEYBLOB, 2, CALG_RSA_KEYX)
+    result += RSAPUBKEY_s.pack(RSAPUBKEY_MAGIC, len(n_bytes) * 8, e)
+    result += n_bytes
+    return result
+
+
+def rsa_to_privatekeystruc(rsa_key):
+    n = rsa_key.key.n
+    e = rsa_key.key.e
+    d = rsa_key.key.d
+    p = rsa_key.key.p
+    q = rsa_key.key.q
+
+    n_bytes = long_to_bytes(n)[::-1]
+    key_len = len(n_bytes) * 8
+    result = PUBLICKEYSTRUC_s.pack(bType_PRIVATEKEYBLOB, 2, CALG_RSA_KEYX)
+    result += RSAPUBKEY_s.pack(PRIVATEKEYBLOB_MAGIC, key_len, e)
+    result += n_bytes
+    result += long_to_bytes(p, key_len / 16)[::-1]
+    result += long_to_bytes(q, key_len / 16)[::-1]
+    result += long_to_bytes(d % (p - 1), key_len / 16)[::-1]
+    result += long_to_bytes(d % (q - 1), key_len / 16)[::-1]
+    result += long_to_bytes(inverse(q, p), key_len / 16)[::-1]
+    result += long_to_bytes(d, key_len / 8)[::-1]
+    return result
+
+
+def import_publickeystruc(data):
+    publickeystruc = PUBLICKEYSTRUC._make(PUBLICKEYSTRUC_s.unpack_from(data))
+    if publickeystruc.bType == bType_PUBLICKEYBLOB and publickeystruc.bVersion == 2 and \
+                    publickeystruc.aiKeyAlg == CALG_RSA_KEYX:
+        rsapubkey = RSAPUBKEY._make(RSAPUBKEY_s.unpack_from(data[8:]))
+        if rsapubkey.magic == RSAPUBKEY_MAGIC:
+            bitlen8 = rsapubkey.bitlen / 8
+            modulus = bytes_to_long(data[20:20 + bitlen8][::-1])
+            r = RSA.construct((modulus, long(rsapubkey.pubexp)))
+            return r
+
+    if publickeystruc.bType == bType_PRIVATEKEYBLOB and publickeystruc.bVersion == 2 and \
+                    publickeystruc.aiKeyAlg == CALG_RSA_KEYX:
+        rsapubkey = RSAPUBKEY._make(RSAPUBKEY_s.unpack_from(data[8:]))
+        if rsapubkey.magic == PRIVATEKEYBLOB_MAGIC:
+            bitlen8 = rsapubkey.bitlen / 8
+            bitlen16 = rsapubkey.bitlen / 16
+            privatekeyblob_s = struct.Struct(
+                '%ds%ds%ds%ds%ds%ds%ds' % (bitlen8, bitlen16, bitlen16, bitlen16, bitlen16, bitlen16, bitlen8))
+            privatekey = PRIVATEKEYBLOB._make(bytes_to_long(x[::-1]) for x in privatekeyblob_s.unpack_from(data[20:]))
+
+            r = RSA.construct((privatekey.modulus, long(rsapubkey.pubexp), privatekey.privateExponent,
+                               privatekey.prime1, privatekey.prime2))
+            return r
+    raise NotImplementedError('Microsoft key type not yet implemented')
--- wincrypto/tests/__init__.py
+++ wincrypto/tests/__init__.py
@@ -1,12 +1,46 @@
 import unittest
 
+from Crypto.PublicKey import RSA
+
 import wincrypto
 
 
-class TestJoke(unittest.TestCase):
-    def test_is_string(self):
-        s = wincrypto.test()
-        self.assertTrue(isinstance(s, basestring))
+TEST_RSA_PRIVATE_KEY = '''-----BEGIN RSA PRIVATE KEY-----
+MIICXQIBAAKBgQCaAuiiYlR++UcrvGWdBXLJc2DIFtOXHK8ZSwLf8U279sLuh4Cz
+LY9iT0i9ABqakJ3rPw/zV4ZizvWhkZuyx9/uWM7a27NLizObr8oi+CEGIPgnFCqE
+//+Nk82mR5EPeCZ6ofZwuPsC/JUlcC+BtUfT7a3oWMnOk3CjFwYAdXVT0QIDAQAB
+AoGAQjCZ1qA3/FIaGlvft/pNePLTV2soCLvVLSOl2qUUaYIGCQnHcDpWc0Pr2P6g
+cGlS1XXG3yhwoyqbOpMfvVhVjqjOUvH71BZJbxyXoXeuvk6GOKivadqCfdAqGX8V
+zAzXVw2vk8D9BzQYS1DWirynSjL76H/d24A99YHvUPG/hzkCQQC1SraaeZufxcJK
+1D+9iRr0aDGgOoZL4fWge8MTortM2YbdUAFbzBVXdfl7Sm9Y9mKLacaPERKbrAFh
+ikhjdFybAkEA2XpBTON38t5EXxBon/hMV4MT+8x9DyXJ87h3/yq0ojnrvgu6RSnD
+ltalpc3KJZ7WO4vP8/IcDjkG/1UxJNmaAwJBAK5oz12zioVeEro8kYm9QkJJjxyP
+0S1lmBGpnxXf44NebkGxu2zd3NZEeBwlkxOqDUoEG/L9QMKk6rs09sk/Y+sCQGAD
+r6zIiH57TuhBkE+ACgRg5IO4pkX3ww+NE71eF13AAKpo9xXt+GIx5fQrxOGTHLYx
+ZeAntec5mjNEY2wHfg0CQQCURZoU82O4RDNofV2iMoQYjMOMBTusHXE+RBb0WLhu
+opeJSYOOCFE/fRWapqXo1TIEDlCkYYlufIa6FkFc/wAL
+-----END RSA PRIVATE KEY-----'''
+
+TEST_RSA_PUBLIC_KEY = '''-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCaAuiiYlR++UcrvGWdBXLJc2DI
+FtOXHK8ZSwLf8U279sLuh4CzLY9iT0i9ABqakJ3rPw/zV4ZizvWhkZuyx9/uWM7a
+27NLizObr8oi+CEGIPgnFCqE//+Nk82mR5EPeCZ6ofZwuPsC/JUlcC+BtUfT7a3o
+WMnOk3CjFwYAdXVT0QIDAQAB
+-----END PUBLIC KEY-----'''
+
+
+class TestRsaKeys(unittest.TestCase):
+    def test_rsa_public_import(self):
+        pycrypto_key = RSA.importKey(TEST_RSA_PUBLIC_KEY)
+        ms_key = wincrypto.rsa_to_publickeystruc(pycrypto_key)
+        pycrypto_key2 = wincrypto.import_publickeystruc(ms_key)
+        self.assertEqual(pycrypto_key, pycrypto_key2)
+
+    def test_rsa_private_import(self):
+        pycrypto_key = RSA.importKey(TEST_RSA_PRIVATE_KEY)
+        ms_key = wincrypto.rsa_to_privatekeystruc(pycrypto_key)
+        pycrypto_key2 = wincrypto.import_publickeystruc(ms_key)
+        self.assertEqual(pycrypto_key, pycrypto_key2)
 
 
 if __name__ == '__main__':
