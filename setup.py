e5fe6568ac70ea896a8596519658a990f13a9982
crappycrypto <crappycrypto@xs4all.nl>
2020-10-31 13:09:21 +0100

    Update to v0.3

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from setuptools import setup
 
-
 setup(name='wincrypto',
-      version='0.2a',
+      version='0.3',
       description='Windows Crypto API compatible decryption/encryption',
       url='http://github.com/crappycrypto/wincrypto',
       author='crappycrypto',
@@ -17,8 +16,9 @@ setup(name='wincrypto',
           "Development Status :: 3 - Alpha",
           "Topic :: Security :: Cryptography",
           "License :: OSI Approved :: MIT License",
-          "Programming Language :: Python :: 2.6",
           "Programming Language :: Python :: 2.7",
-          "Programming Language :: Python :: 3.3",
-          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "Programming Language :: Python :: 3.8",
       ])
